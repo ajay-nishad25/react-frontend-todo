d69ddef268bcabad472971ab54c18218643ae28d
Ajay Nishad <ajaynishad4123@gmail.com>
2026-01-09 20:15:25 +0530

    add update todo code

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -27,13 +27,23 @@ export function getTodosApi(page, debouncedSearch) {
 }
 
 export function deleteTodoApi(todoId) {
-  console.log("todoId", todoId);
   return api
     .delete("/delete-todo/", {
       params: {
         todo_id: todoId,
       },
     })
+    .then((res) => {
+      return Promise.resolve(res?.data);
+    })
+    .catch((error) => {
+      return Promise.reject(error?.response?.data?.error);
+    });
+}
+
+export function updateTodoApi(updateData) {
+  return api
+    .patch("/update-todo/", updateData)
     .then((res) => {
       return Promise.resolve(res?.data);
     })
